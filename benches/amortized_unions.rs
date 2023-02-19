ea517b5281c3bc6902ffa78d414b49db13eba129
Yannissss <yannis.ondars@gmail.com>
2023-02-19 14:43:35 +0100

    Adding benchmark

---
+++ benches/amortized_unions.rs
@@ -0,0 +1,23 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use union_find::Repr;
+
+fn sum_bench(c: &mut Criterion) -> &mut Criterion {
+    c.bench_function("amortized_unions / 10_000 elements", |b| {
+        b.iter(|| {
+            let mut root = Repr::new(-1);
+
+            let mut _nodes = Vec::new();
+
+            for k in 0..10_000 {
+                let mut node = Repr::new(k);
+                root.union(&mut root);
+                _nodes.push(node);
+            }
+        })
+    });
+    c
+}
+
+criterion_group!(benches, sum_bench);
+criterion_main!(benches);
